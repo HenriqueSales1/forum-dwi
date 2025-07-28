7cfcb2fff01a26188b71780f6464551bf7030a7c
Henrique <henriquesabsales912@gmail.com>
2025-07-27 23:11:19 -0300

    update burrice pt 3

--- frontend/forum-frontend/src/components/NavBar.jsx
+++ frontend/forum-frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import "./NavBar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
