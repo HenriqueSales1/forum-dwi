fe7ecf5c32c6253d62d6be42cf87bf699ace623d
Henrique <henriquesabsales912@gmail.com>
2025-07-27 21:32:11 -0300

    render db key

--- backend/database/mysql.js
+++ backend/database/mysql.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  host: "localhost",
-  port: 3306, 
-  dialect: "mysql",
-  database: "forum", 
-  username: "root", 
-  password: "root", 
-});
+// const sequelize = new Sequelize({
+//   host: "localhost",
+//   port: 3306, 
+//   dialect: "mysql",
+//   database: "forum", 
+//   username: "root", 
+//   password: "root", 
+// });
+
+const sequelize = new Sequelize ('postgresql://forum_dwi_user:5LsIEY6gcmPHGwIeYyt0Ga9lzRlLVS7t@dpg-d23c9eu3jp1c739p46jg-a/forum_dwi_db')
 
 export default sequelize;
